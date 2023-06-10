d12453a7ad2bdb0b9f85d87a201a0f119f313315
Harith Abeysinghe <e19003@eng.pdn.ac.lk>
2023-06-10 16:57:05 +0530

    Initial

--- recognizing_images.py
+++ recognizing_images.py
@@ -1,30 +1,19 @@
-import numpy as np
-import os
-import PIL
-import PIL.Image
-import tensorflow as tf
-from keras.models import load_model
+import pytesseract as tess
+from PIL import Image
+from pytesseract import pytesseract
 
-'''
-1. Fetching data from image
-This program is used to detect letter from text and convert it into data 
-'''
+pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
+def recognize(path):
+    img = Image.open('test.jpg')
+    text = tess.image_to_string(img)
+    return text
 
-# Load the text detection and recognition models.
-text_detection_model = load_model("path/to/text_detection_model")
-text_recognition_model = load_model("path/to/text_recognition_model")
+print(recognize("test.jpg"))
+print(recognize("test2.jpg"))
+print(recognize("test3.jpg"))
+print(recognize("test4.jpg"))
+print(recognize("test5.jpg"))
+print(recognize("test6.jpg"))
+print(recognize("cameraphoto.jpg"))
 
-# Read the image into memory.
-image = tf.keras.preprocessing.image.load_img("test.jpeg")
-
-# Detect the text in the image.
-text_boxes = text_detection_model.predict(image)
-
-# Recognize the text in the image.
-recognized_text = []
-for text_box in text_boxes:
-    recognized_text.append(text_recognition_model.predict(text_box))
-
-# Print the recognized text.
-print(recognized_text)
