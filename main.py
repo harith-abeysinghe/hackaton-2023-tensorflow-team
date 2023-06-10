d4401bfadbcd8cc2ee2cb688d5446c72d8a0d1bd
Harith Abeysinghe <e19003@eng.pdn.ac.lk>
2023-06-10 13:25:44 +0530

    Initial

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import os
+import PIL
+import PIL.Image
+import tensorflow as tf
+import tensorflow_datasets as tfds
+'''
+1. Fetching data from image
+2. Creating a model
+
+'''
